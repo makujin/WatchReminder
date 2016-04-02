a1affdc1b50d2f4cfac3bb2e59de6721dcd5d599
makujin <nikitos83@yahoo.com>
2016-04-03 01:43:58 +0600

    Let's get this show on a road!

---
+++ WatchReminder/App_Start/BundleConfig.cs
@@ -0,0 +1,35 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace WatchReminder
+{
+    public class BundleConfig
+    {
+        //Дополнительные сведения об объединении см. по адресу: http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-1.10.2.js",
+                        "~/Scripts/jquery-1.10.2.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Используйте версию Modernizr для разработчиков, чтобы учиться работать. Когда вы будете готовы перейти к работе,
+            // используйте средство сборки на сайте http://modernizr.com, чтобы выбрать только нужные тесты.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                   
+                "~/Scripts/bootstrap.min.js",
+                      "~/Scripts/jasny-bootstrap.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                    
+                     "~/Content/bootstrap.min.css",
+                      "~/Content/jasny-bootstrap.min.css",
+                      "~/Content/Site.css"));
+        }
+    }
+}
---
+++ WatchReminder/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется следующим образом
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных с этой сборкой.
+[assembly: AssemblyTitle("WatchReminder")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WatchReminder")]
+[assembly: AssemblyCopyright("© , 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False в параметре ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM.  Если требуется обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение true для требуемого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для ID библиотеки типов typelib, если этот проект видим для COM
+[assembly: Guid("56663e80-e4a7-48f9-98b9-c053c8df4b24")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      основной номер версии;
+//      Дополнительный номер версии
+//      номер сборки;
+//      редакция.
+//
+// Можно задать все значения или принять номер редакции и номер сборки по умолчанию,
+// используя "*", как показано ниже:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
